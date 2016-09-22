58938ebf83ae2aa2c2c4e712156a4eeb6bfb3bd3
Chris Beauclair <chris.beauclair@gmail.com>
2016-09-22 19:40:05 -0400

    Everything working properly, but it aint pretty :/

--- lab1.pl
+++ lab1.pl
@@ -8,29 +8,41 @@ use Crypt::Random qw( makerandom );
 
 my $symetrickey = makerandom( Size => 128, Strength => 1);
 my $md5 = Digest::MD5->new;
-my $cipher = Crypt::CBC->new( -key => $symetrickey);
+my $aliceCipher = Crypt::CBC->new( -key => $symetrickey);
 my $rsa = new Crypt::RSA;
 local $/ = undef;
 open(my $fh, "<", "test.txt");	
 open(my $ofh, ">", "testEncrypt.txt");
 my $h1;
+my $encryptedKey;
 my ($alicePublic, $alicePrivate) = $rsa->keygen(Size => 3072);
 my ($bobPublic, $bobPrivate) = $rsa->keygen(Size => 3072);
+#Alice Actions
 while(my $line = <$fh>) {	
-	print $ofh $cipher->encrypt($line);
+	print $ofh $aliceCipher->encrypt($line);
 	$md5->add($line);
 	$h1 = $md5->digest();
-	print $h1;
-	#print $ofh $rsa->encrypt(Message => $line,Key => $bobPublic);
+	$encryptedKey = $rsa->encrypt(Message => $symetrickey,Key => $bobPublic);
 }
+my $aliceSignature = $rsa->sign(Message=>$h1, Key =>$alicePrivate);
+
+
 close $ofh;
 open($ofh, ">", "testdecrypt.txt");
 open(my $encryptedFileHandle, "<", "testEncrypt.txt");
-
+#BobActions
 while (my $line = <$encryptedFileHandle>) {
-	my $decryptedText = $cipher->decrypt($line);
+	my $decyptedKey = $rsa->decrypt (Cyphertext=> $encryptedKey, Key=>$bobPrivate);
+	my $bobCipher = Crypt::CBC->new( -key => $decyptedKey);
+	my $decryptedText = $bobCipher->decrypt($line);
 	$md5->add($decryptedText);
 	my $h2 = $md5->digest();
+	my $h = $rsa->verify(Message =>$h2, Signature=> $aliceSignature, Key=>$alicePublic);
+	if($h == 1){
+		print "Hash is good, and Signature matches\n";
+	}else {
+		print "ermagerd you've been hacked GASP\n";
+	}
 	if($h2 eq $h1){
 		print "they are the sammmme \n";
 	}
