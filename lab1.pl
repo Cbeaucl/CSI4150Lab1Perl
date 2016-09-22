6e5e95cd19816439baee996556db1e3f0c1d85d5
Chris Beauclair <chris.beauclair@gmail.com>
2016-09-22 18:45:34 -0400

    Added hashing and changed file text encryption to use semetric key encryption at 128bit.

--- lab1.pl
+++ lab1.pl
@@ -3,21 +3,24 @@ use strict;
 use warnings;
 use Crypt::RSA;
 use Crypt::CBC;
+use Digest::MD5;
 use Crypt::Random qw( makerandom );
 
 my $symetrickey = makerandom( Size => 128, Strength => 1);
-print $symetrickey;
-my $cipher = new Crypt::CBC->new(-key => $symetrickey);
-print "hi NAME\n";
+my $md5 = Digest::MD5->new;
+my $cipher = Crypt::CBC->new( -key => $symetrickey);
+my $rsa = new Crypt::RSA;
 local $/ = undef;
 open(my $fh, "<", "test.txt");	
 open(my $ofh, ">", "testEncrypt.txt");
-my $rsa = new Crypt::RSA;
-my ($alicePublic, $alicePrivate) = $rsa->keygen(Size => 1024);
-my ($bobPublic, $bobPrivate) = $rsa->keygen(Size => 1024);
-
+my $h1;
+my ($alicePublic, $alicePrivate) = $rsa->keygen(Size => 3072);
+my ($bobPublic, $bobPrivate) = $rsa->keygen(Size => 3072);
 while(my $line = <$fh>) {	
 	print $ofh $cipher->encrypt($line);
+	$md5->add($line);
+	$h1 = $md5->digest();
+	print $h1;
 	#print $ofh $rsa->encrypt(Message => $line,Key => $bobPublic);
 }
 close $ofh;
@@ -25,7 +28,13 @@ open($ofh, ">", "testdecrypt.txt");
 open(my $encryptedFileHandle, "<", "testEncrypt.txt");
 
 while (my $line = <$encryptedFileHandle>) {
-	print $ofh $cipher->decrypt($line);
+	my $decryptedText = $cipher->decrypt($line);
+	$md5->add($decryptedText);
+	my $h2 = $md5->digest();
+	if($h2 eq $h1){
+		print "they are the sammmme \n";
+	}
+	print $ofh $decryptedText;
 	#print $ofh $rsa->decrypt(Cyphertext => $line, Key => $bobPrivate);
 }
 close $fh;
